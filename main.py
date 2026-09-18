2b50f697cf03c7e69a2783494e370f65143ba9b1
Fa <amjafamo@gmail.com>
2026-09-18 11:53:39 +0330

    Add text to Morse converter

---
+++ main.py
@@ -0,0 +1,34 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ',': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+user_input = input(" type the txt :").upper()
+
+
+def text_to_morse(user_input):
+    morse_list = []
+    for letter in user_input:
+        if letter == " ":
+            morse_list.append(" ")
+        elif letter not in MORSE_CODE_DICT:
+            morse_list.append(letter)
+        else:
+            morse_list.append(MORSE_CODE_DICT[letter])
+
+    return morse_list
+
+
+result = text_to_morse(user_input)
+print(f"The morse code is : \n {' '.join(result)}")
